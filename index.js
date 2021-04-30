dd963d32ebf165286e4add1d2b1abe9de221ad81
ifcodes <ifeco133@gmail.com>
2021-04-30 08:01:03 +0100

    update

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const { response, request } = require("express");
 // const { response } = require("express");
 const app = express();
-const cors = require(cors);
+const cors = require("cors");
 
 app.use(cors());
 app.use(express.json());
